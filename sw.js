462f08964881742b3f41881140c5b94df83c31c9
elisagir <girgenti.elisa@gmail.com>
2019-02-09 16:11:55 +0100

    Initial commit

---
+++ sw.js
@@ -0,0 +1,48 @@
+let staticCacheName = 'restaurant-static-v1';
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+    return cache.addAll([
+      'index.html',
+      'restaurant.html',
+      '/css/main.css',
+      '/css/responsive.css',
+      '/js/dbhelper.js',
+      '/js/main.js',
+      '/js/restaurant_info.js',
+      '/img/*',
+      '/js/sw_register.js',
+      '//normalize-css.googlecode.com/svn/trunk/normalize.css',
+      'https://fonts.googleapis.com/css?family=Open+Sans:400,600,700'
+    ])
+    .catch(error => {
+
+    });
+  }));
+});
+
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys()
+		.then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.filter(function(cacheName) {
+					return cacheName.startsWith('restaurant-') &&
+						   cacheName != staticCacheName;
+				}).map(function(cacheName) {
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
+})
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
